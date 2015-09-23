6f08f8f74196368c88bf226decd64ee9c83f1289
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-23 23:20:04 +0100

    Working backup script + unit files

--- backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-
-sudo -u postgres pg_dump --compress=0 --exclude-table=cert --exclude-table=log_entry ct-watch > db.dump
-tarsnap -c -v --keyfile ~/.tarsnap.ctwatch.write.key -f database.$(date -u +"%Y%m%dT%H%M%SZ") db.dump
